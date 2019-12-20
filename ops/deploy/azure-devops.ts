6cdda1718673f960826fe09ef3a55cb0d9f98799
jeff-snyk <jeff@snyk.io>
2019-12-19 23:50:55 -0500

    feat: initial implementation of deployment script to be used for deploying locally (to test envs) as well as integrated into the CI/CD for automatic deployment.

---
+++ ops/deploy/azure-devops.ts
@@ -0,0 +1,66 @@
+import * as nodeApi from 'azure-devops-node-api';
+import * as ExtensionManagementApi from 'azure-devops-node-api/ExtensionManagementApi';
+import * as ExtensionManagementInterfaces from 'azure-devops-node-api/interfaces/ExtensionManagementInterfaces';
+import * as lim from "azure-devops-node-api/interfaces/LocationsInterfaces";
+
+function getEnv(name: string): string {
+  const val = process.env[name];
+  if (!val) {
+      console.error(`${name} env var not set`);
+      process.exit(1);
+  }
+  return val;
+}
+
+export async function getApi(serverUrl: string, azureDevOpsToken: string): Promise<nodeApi.WebApi> {
+  return new Promise<nodeApi.WebApi>(async (resolve, reject) => {
+      try {
+          const token = azureDevOpsToken;
+          const authHandler = nodeApi.getPersonalAccessTokenHandler(token);
+          const option = undefined;
+
+          const vsts: nodeApi.WebApi = new nodeApi.WebApi(serverUrl, authHandler, option);
+          const connData: lim.ConnectionData = await vsts.connect();
+          if (connData) {
+              if (connData.authenticatedUser) {
+                  console.log(`Hello ${connData.authenticatedUser.providerDisplayName}`);
+              }
+          }
+          resolve(vsts);
+      }
+      catch (err) {
+          reject(err);
+      }
+  });
+}
+
+export async function getWebApi(serverUrl: string, azureDevOpsToken: string): Promise<nodeApi.WebApi> {
+  return await getApi(serverUrl, azureDevOpsToken);
+}
+
+export async function getExtensionInfo(serverUrl: string, azureDevOpsToken: string, publisherName: string, extensionId: string): Promise<ExtensionManagementInterfaces.InstalledExtension> {
+    const webApi: nodeApi.WebApi = await getWebApi(serverUrl, azureDevOpsToken);
+    const extensionManagementApiObject: ExtensionManagementApi.IExtensionManagementApi = await webApi.getExtensionManagementApi();
+    // console.log('States:', await extensionManagementApiObject.getStates(true, true, true));
+
+    // Although this API claims to be "ByName", it actually corresponds to the the `extensionId`. The same weirdness exists when you use the az devops CLI
+    return extensionManagementApiObject.getInstalledExtensionByName(publisherName, extensionId);
+}
+
+export async function uninstallExtension(serverUrl: string, azureDevOpsToken: string, publisherName: string, extensionId: string): Promise<void> {
+    const webApi: nodeApi.WebApi = await getWebApi(serverUrl, azureDevOpsToken);
+    const extensionManagementApiObject: ExtensionManagementApi.IExtensionManagementApi = await webApi.getExtensionManagementApi();
+    // console.log('States:', await extensionManagementApiObject.getStates(true, true, true));
+
+    // Although this API claims to be "ByName", it actually corresponds to the the `extensionId`. The same weirdness exists when you use the az devops CLI
+    return extensionManagementApiObject.uninstallExtensionByName(publisherName, extensionId);
+}
+
+export async function installExtension(serverUrl: string, azureDevOpsToken: string, publisherName: string, extensionId: string): Promise<ExtensionManagementInterfaces.InstalledExtension>{
+  const webApi: nodeApi.WebApi = await getWebApi(serverUrl, azureDevOpsToken);
+  const extensionManagementApiObject: ExtensionManagementApi.IExtensionManagementApi = await webApi.getExtensionManagementApi();
+  // console.log('States:', await extensionManagementApiObject.getStates(true, true, true));
+
+  // Although this API claims to be "ByName", it actually corresponds to the the `extensionId`. The same weirdness exists when you use the az devops CLI
+  return extensionManagementApiObject.installExtensionByName(publisherName, extensionId);
+}
---
+++ ops/deploy/cli-args.ts
@@ -0,0 +1,120 @@
+const yargs = require("yargs");
+const fs = require("fs");
+
+enum Command {
+  VersionCheck,
+  Deploy
+}
+
+enum DeployTarget {
+    Prod,
+    Dev,
+    Custom
+}
+
+interface IArgs {
+  command: Command,
+  target: DeployTarget,
+  configFile: string,
+  newVersion: string
+}
+
+const parseInputParameters = (inputArgs): IArgs => {
+  const scriptName = "deploy";
+  const usageMsg = "Usage: $0 <command>";
+  const testCommandUsage = "test <chart-directory> [options]";
+  const testCommandDescription = "Check images in your charts for vulnerabilities";
+  const scriptExample = "$0 test . --output=snyk-out.json";
+
+  const argv = yargs(inputArgs)
+  .version()
+  .scriptName(scriptName)
+  .usage(usageMsg)
+  .command("version-check", "Checks that all the versions match")
+  .command('deploy [options]', 'Deploys to specified target', y => {
+    y.option('target', {
+      description: 'the deployment target',
+      type: 'string',
+      demandOption: true,
+      choices: ['dev', 'prod', 'custom']
+    })
+    .option('config-file', {
+      description: 'Config JSON file specifying custom deployment arguments',
+      type: 'string'
+    })
+    .option('new-version', {
+      description: 'New version to release; only valid with --target=custom',
+      type: 'string'
+    })
+    .check(argsToCheck => {
+      if (argsToCheck.target === 'custom' && !argsToCheck.configFile) {
+        throw new Error("--config-file is required for target `custom`");
+      }
+
+      if (argsToCheck.target !== 'custom' && argsToCheck.configFile) {
+        throw new Error("--config-file isonly allowed with target `custom`");
+      }
+
+      if (argsToCheck.target !== 'custom' && argsToCheck.newVersion) {
+        throw new Error("--new-version is only allowed with target `custom`");
+      }
+
+      return true;
+
+    });
+  })
+  .demandCommand(1)
+  .help("help")
+  .alias("help", "h")
+  .example("$0 version-check")
+  .example("$0 deploy --target=dev")
+  .example("$0 deploy --target=prod")
+  .example("$0 deploy --target=custom --config-file=custom-args.json")
+  .argv;
+
+  console.log("argv:", argv);
+
+  return parseOptions(argv);
+};
+
+const parseOptions = (argv: any): IArgs => {
+  const options = {
+    command: Command.VersionCheck,
+    target: DeployTarget.Custom,
+    configFile: "",
+    newVersion: ""
+  } as IArgs;
+
+  const command = argv._[0];
+  if (command === "version-check") {
+    options.command = Command.VersionCheck;
+  } else if (command === "deploy") {
+    options.command = Command.Deploy;
+
+    if (argv.target) {
+      if (argv.target === "dev") {
+        options.target = DeployTarget.Dev;
+      } else if (argv.target === "prod") {
+        options.target = DeployTarget.Prod;
+      } else if (argv.target === "custom") {
+        options.target = DeployTarget.Custom;
+        if (argv.configFile) {
+          options.configFile = argv.configFile;
+        }
+        if (argv.newVersion) {
+          options.newVersion = argv.newVersion;
+        }
+      } else {
+        // this should never happen
+        throw new Error(`Invalid command: ${command}`);
+      }
+    }
+  } else {
+    // this should never happen
+    throw new Error(`Invalid deploy target: ${argv.target}`);
+  }
+
+  return options;
+};
+
+export { IArgs, parseInputParameters, Command, DeployTarget };
