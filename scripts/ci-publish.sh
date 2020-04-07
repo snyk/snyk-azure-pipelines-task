5a91ec171847d7f19b31fce6e0452b8c071a404f
Arthur Granado <arthur.granado@snyk.io>
2020-04-07 13:51:14 +0100

    chore: automate the deployment process

---
+++ scripts/ci-publish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script publish the extension to Marketplace. Production deployment.
+# Arguments:
+# $1 - Extension version
+
+# Handle arguments
+AZ_EXT_NEW_VERSION="$1"
+
+# Updating version in task.json file
+node "${PWD}/scripts/ci-update-task-json-prod.js" ${AZ_EXT_NEW_VERSION}
+
+# Override version
+OVERRIDE_JSON="{ \"id\": \"${AZ_EXTENSION_ID}\", \"name\": \"${AZ_EXTENSION_NAME}\", \"version\": \"${AZ_EXT_NEW_VERSION}\", \"public\": true }"
+
+# Publish extension
+echo "Publishing extension..."
+tfx extension publish --manifest-globs vss-extension.json \
+--version $AZ_EXT_NEW_VERSION \
+--extension-id $AZ_EXTENSION_ID \
+--publisher $AZ_PUBLISHER \
+--override $OVERRIDE_JSON \
+--token $AZURE_DEVOPS_EXT_PAT
+
+echo "Extension published"
---
+++ scripts/recovery-task-json-dev.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const filePath = "./snykTask/task.json";
+const fileBakPath = "./snykTask/task.json.bak";
+
+console.log("Recovery snykTask/task.json file...");
+const taskJSON_File = JSON.parse(fs.readFileSync(fileBakPath, "utf8"));
+fs.writeFileSync(filePath, JSON.stringify(taskJSON_File, null, 2), "utf8");
+console.log("File recovered");
