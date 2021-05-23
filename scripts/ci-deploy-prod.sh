f848581aeb0e7f3e0d75ef39c888b79471213bca
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-05-23 10:47:41 -0400

    chore: create and publish in one command

--- scripts/ci-deploy-prod.sh
+++ scripts/ci-deploy-prod.sh
@@ -13,37 +13,19 @@ node "${PWD}/scripts/ci-update-task-json-prod.js" ${AZ_EXT_NEW_VERSION}
 # Override version
 OVERRIDE_JSON="{ \"id\": \"${AZ_EXTENSION_ID}\", \"name\": \"${AZ_EXTENSION_NAME}\", \"version\": \"${AZ_EXT_NEW_VERSION}\", \"public\": true }"
 
-echo "Creating extension..."
-tfx extension create \
---manifest-globs vss-extension.json \
+# Publish extension
+echo "Publishing extension..."
+tfx extension publish --manifest-globs vss-extension.json \
 --version $AZ_EXT_NEW_VERSION \
 --extension-id $AZ_EXTENSION_ID \
 --publisher $AZ_PUBLISHER \
 --override $OVERRIDE_JSON \
---token $AZURE_DEVOPS_EXT_PAT \
---output-path Snyk-snyk-security-scan.vsix
-
-create_exit_code=$?
-if [[ create_exit_code -eq 0 ]]; then
-  echo "Extension created"
-  ls -la
-else
-  echo "Failed to create extension with exit code ${create_exit_code}"
-  exit ${create_exit_code}
-fi
-
-if [[ ! -f "Snyk-snyk-security-scan.vsix" ]]; then
-  echo "missing Snyk-snyk-security-scan.vsix file, not publishing extension"
-  exit 1
-fi
-
-echo "Snyk-snyk-security-scan.vsix file exists, publishing extension..."
-tfx extension publish --token $AZURE_DEVOPS_EXT_PAT --vsix Snyk-snyk-security-scan.vsix
+--token $AZURE_DEVOPS_EXT_PAT
 
 publish_exit_code=$?
 if [[ publish_exit_code -eq 0 ]]; then
   echo "Extension published"
 else
-  echo "Failed to publish extension with exit code ${publish_exit_code}"
+  echo "Extension failed to pubish with exit code ${publish_exit_code}"
   exit ${publish_exit_code}
 fi
